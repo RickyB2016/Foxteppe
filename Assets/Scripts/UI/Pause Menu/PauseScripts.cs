fb2eaccd54175f53fe592fcd760ec4d83971c204
rbarkley4 <1007553@student.sae.edu.au>
2016-11-28 11:56:08 +1100

    Foxteppe UI Build

---
+++ Assets/Scripts/UI/Pause Menu/PauseScripts.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine; 
+using UnityEngine.SceneManagement;
+
+public class PauseScripts : MonoBehaviour {
+
+    public GameObject ui;
+
+	// Update is called once per frame
+	
+  void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+                Toggle();
+            
+        }
+	
+	}
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;    
+        }
+    }
+
+    public void Quit () 
+    {
+        SceneManager.LoadScene ("Scene Name");
+    }
+
+    public void ReturnToMenu(){
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
